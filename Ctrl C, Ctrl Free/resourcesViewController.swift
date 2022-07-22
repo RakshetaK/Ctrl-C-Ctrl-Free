99c9e64518ceeb6a2c99d42a9e3ff1351799dc78
Rui Weng <ruiwengc@gmail.com>
2022-07-21 21:36:56 -0700

    resources done? eh design

--- Ctrl C, Ctrl Free/resourcesViewController.swift
+++ Ctrl C, Ctrl Free/resourcesViewController.swift
@@ -12,18 +12,35 @@ class resourcesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func articleButton(_ sender: Any) {
+        let url = URL(string:"https://www.hsi.org/issues/animal-testing/")!
+        UIApplication.shared.open(url)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func articleButton2(_ sender: Any) {
+        let url = URL(string:"https://www.hsi.org/issues/be-cruelty-free/")!
+        UIApplication.shared.open(url)
     }
-    */
-
+    
+    @IBAction func articleButton3(_ sender: Any) {
+        let url = URL(string:"https://www.hsi.org/issues/biomedical-research/")!
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func videoButton(_ sender: Any) {
+        let url = URL(string:"https://www.youtube.com/watch?v=G393z8s8nFY&t=83s")!
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func pledgeButton(_ sender: Any) {
+        let url = URL(string:"https://action.hsi.org/page/25047/petition/1?ea.tracking.id=website")!
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func donateButton(_ sender: Any) {
+        let url = URL(string:"https://donate.hsi.org/page/23410/donate/1?ea.tracking.id=web-aanm")!
+        UIApplication.shared.open(url)
+    }
 }
